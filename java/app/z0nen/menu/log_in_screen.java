d44241bdafbfad78fdf5e8f80772abf2fde2268b
bgtodp <bridget.odwyer@gmail.com>
2016-03-15 17:19:30 +0000

    integrated gauge with nav menu 2

---
+++ java/app/z0nen/menu/log_in_screen.java
@@ -0,0 +1,107 @@
+package app.z0nen.menu;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import app.z0nen.slidemenu.MyActivity;
+import app.z0nen.slidemenu.R;
+
+public class log_in_screen extends Activity {
+
+    private EditText emailField, passwordField;
+    private TextView oilLevel;
+    private Button viewGaugeButton;
+    String oilLevelFromDataBase;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_screen);
+
+        emailField = (EditText) findViewById(R.id.editTextEmail);
+        passwordField = (EditText) findViewById(R.id.editTextPassword);
+
+        oilLevel = (TextView) findViewById(R.id.textViewLevel);
+        viewGaugeButton = (Button) findViewById(R.id.btnViewGauge);
+
+        TextWatcher watcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                viewGaugeButton.setVisibility(View.VISIBLE);
+            }
+        };
+
+        oilLevel.addTextChangedListener(watcher);
+
+    }
+
+    public void loginPost(View view){
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
+
+        new sign_in(this, oilLevel, 1).execute(email, password);
+
+        oilLevelFromDataBase = oilLevel.getText().toString();
+
+    }
+
+    public void viewGaugePage(View view){
+
+        Intent viewGaugeIntent = new Intent(log_in_screen.this,MyActivity.class);
+
+        String tankLevelFromDatabase = oilLevel.getText().toString();
+        String tankUpdate = null;
+        viewGaugeIntent.putExtra(tankUpdate, tankLevelFromDatabase);
+        startActivity(viewGaugeIntent);
+        log_in_screen.this.startActivity(viewGaugeIntent);
+        log_in_screen.this.finish();
+
+    }
+
+    //getting value back from gauge
+    public interface AsyncResponse{
+        void processFinish(String databaseValue);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_log_in_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- java/app/z0nen/slidemenu/menu1_Fragment.java
+++ java/app/z0nen/slidemenu/menu1_Fragment.java
@@ -2,20 +2,59 @@ package app.z0nen.slidemenu;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import io.sule.gaugelibrary.GaugeView;
+import android.view.Menu;
+import android.view.MenuItem;
+import java.util.Random;
 
 /**
  * Created by Z0NEN on 10/22/2014.
  */
 public class menu1_Fragment extends Fragment {
-    View rootview;
+    private View rootview;
+    String tankLevelFromLogIn;
+    private TextView gaugeValue;
+    private GaugeView mGaugeView;
+    private final Random RAND = new Random();
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootview = inflater.inflate(R.layout.menu1_layout, container, false);
+
+
+
+        String tankUpdate = null;
+        mGaugeView = (GaugeView) getView().findViewById(R.id.gauge_view);
+        mTimer.start();
+        Bundle bundle = getActivity().getIntent().getExtras();
+
+        gaugeValue = (TextView)getView().findViewById(R.id.textViewGaugeLevel);
+
+        tankLevelFromLogIn = bundle.getString(tankUpdate);
+
+        gaugeValue.setText(tankLevelFromLogIn);
+
+
+
         return rootview;
     }
+
+    private final CountDownTimer mTimer = new CountDownTimer(30000, 1000) {
+
+        @Override
+        public void onTick(final long millisUntilFinished) {
+            float tankLevel = Float.parseFloat(tankLevelFromLogIn);
+            mGaugeView.setTargetValue(tankLevel);
+        }
+
+        @Override
+        public void onFinish() {}
+    };
 }
